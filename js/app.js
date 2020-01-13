95594013403fca92c79712ad1b53305c9f0f814c
Ryan Whittemore <ryanwhitt14@gmail.com>
2020-01-13 12:57:06 -0500

    modal changes

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const resetAll = document.querySelector('.reset');
 const appendTimer = document.querySelector('.timer');
 const stars = document.querySelector('.stars');
 const modalStars = document.querySelector('.star-rating');
-const star = '<li><i class="fa fa-star"></i></li> <li><i class="fa fa-star"></i></li> <li><i class="fa fa-star"></i></li>';
+const starRatingWrapper = document.querySelector('.star-rating-wrapper')
 const getCards = Array.from(document.querySelectorAll('.deck li'));
 let count = 0;
 let time = 0;
@@ -112,8 +112,8 @@ function closeModal() {
 function winCondition() {
     if (matchingCards.length === 16) {
         stopTimer();
-        modalCounter.innerHTML = count;
-        setStars(modalStars);
+        modalCounter.innerHTML = `You won in ${count} moves`;
+        setModalStars();
         openModal();
     }
 }
@@ -136,16 +136,42 @@ function displayTimer() {
     const min = Math.floor(time / 60);
     const sec = time % 60;
     if (sec < 10) {
-        appendTimer.innerHTML = `${min}:0${sec}`;
+        appendTimer.innerHTML = `it took you ${min}:0${sec}`;
     } else {
-        appendTimer.innerHTML = `${min}:${sec}`;
+        appendTimer.innerHTML = `it took you ${min}:${sec}`;
+    }
+}
+
+function setModalStars() {
+    if (count < 12) {
+        starRatingWrapper.innerHTML = `
+        <h4>3 Stars?! You're a smart cookie<h4/>
+        <ul class="star-rating">
+        <li><i class="fa fa-star"></i></li>
+        <li><i class="fa fa-star"></i></li>
+        <li><i class="fa fa-star"></i></li>
+    </ul>`
+    } else if (count >= 12 && count < 18) {
+        starRatingWrapper.innerHTML = `
+        <h4>Not bad</h4> 
+        <ul class="star-rating">
+        <li><i class="fa fa-star"></i></li>
+        <li><i class="fa fa-star"></i></li>
+    </ul>`
+    } else {
+        starRatingWrapper.innerHTML = `<h4>Surely you can do better</h4> 
+        <ul class="star-rating">
+        <li><i class="fa fa-star"></i></li>
+    </ul>`;
     }
 }
 
 //Display number of stars on Modal and in game.
 function setStars(elem) {
     if (count < 12) {
-        elem.innerHTML = star;
+        elem.innerHTML = `<li><i class="fa fa-star"></i></li>
+        <li><i class="fa fa-star"></i></li>
+        <li><i class="fa fa-star"></i></li>`;
     } else if (count >= 12 && count < 18) {
         elem.innerHTML = '<li><i class="fa fa-star"></i></li> <li><i class="fa fa-star"></i></li>';
     } else {
@@ -168,7 +194,9 @@ function resetGame() {
     closeModal();
     time = 0;
     timerOn = false;
-    stars.innerHTML = star;
+    stars.innerHTML = `<li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>
+    <li><i class="fa fa-star"></i></li>`
     openCards = [];
     shuffleDeck();
 }
